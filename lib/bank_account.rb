c1b2086df5541e9acd2ef113ebe5dac21f2fb48c
Jamie Vullo <jvullo@protonmail.com>
2019-06-07 22:41:23 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,7 @@ class BankAccount
   
   attr_accessor :name, :balance
   
-  def initialize(name, balance = 1000, open)
+  def initialize(name, balance = 1000)
     @name = name
     @balance = balance 
     @open = open 
